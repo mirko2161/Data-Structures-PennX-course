04f03947d0393c911b9a4ec8ba972eed31fc80fe
mirko <mirko.djurdjevic@ymail.com>
2017-11-10 15:01:56 +0100

    The HashSet now auto-resizes based on the load factor

--- src/data_structures/HashSet.java
+++ src/data_structures/HashSet.java
@@ -3,19 +3,24 @@ package data_structures;
 import java.util.LinkedList;
 
 /**
- * Basic hash set implementation using separate chaining to avoid collisions.
+ * Basic hash set implementation using separate chaining to avoid collisions. The hast set resizes
+ * itself based on the load factor ratio to make sure the access/search etc time stays close to
+ * O(1).
  *
  */
 public class HashSet {
 
     // all the elements with the same hash will be together in a bucket
     private LinkedList<String>[] buckets;
+    private int currentSize = 0;
+    private double loadFactor; // balance between time and space tradeoffs
 
-    public HashSet(int size) {
+    public HashSet(int size, double loadFactor) {
         this.buckets = new LinkedList[size];
         for (int i = 0; i < size; i++) {
             buckets[i] = new LinkedList<>();
         }
+        this.loadFactor = loadFactor;
     }
 
     /**
@@ -30,9 +35,29 @@ public class HashSet {
             int index = hashCode(value) % buckets.length; // mod to prevent going outside of array range
             LinkedList<String> bucket = buckets[index];
             bucket.addFirst(value);
+            currentSize++;
+
+            double averageLoad = currentSize / (double) buckets.length;
+            if (averageLoad > loadFactor) {
+                reinsertAll();
+            }
             return true;
         }
         return false;
+    }
+
+    private void reinsertAll() {
+        LinkedList<String> oldBuckets[] = buckets;
+        buckets = new LinkedList[buckets.length * 2]; // the new array will be twice the size the old one
+
+        for (LinkedList<String> bucket : oldBuckets) {
+            for (String element : bucket) {
+                // because of the new array length some of the element will have a new index
+                int index = hashCode(element) % buckets.length;
+                LinkedList<String> newBucket = buckets[index];
+                newBucket.addFirst(element);
+            }
+        }
     }
 
     public boolean contains(String value) {
